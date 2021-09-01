6b31c83035ad170a2bc846235c79f23405b6b0f2
victoriasof <victoria.sofianidou@gmail.com>
2021-09-01 16:13:59 +0200

    made table appear

--- client/src/components/customers.js
+++ client/src/components/customers.js
@@ -13,22 +13,52 @@ class Customers extends Component {
     fetch('/api/customers')
       .then(res => res.json())
       .then(customers => {
-        console.log(customers);
+        //console.log(customers);
 
         this.setState({customers}, () => console.log('Customers fetched...', customers))
       });
   }
 
   render() {
+    console.log(this.state.customers);
+
     return (
       <div>
-        <h2>Customers</h2>
+
+        {/* <h2>Customers</h2>
         <ul>
-          {/* ΔΕΙΞΕ ΕΔΩ ΤΟ TABLE ΚΑΙ ΤΡΑΒΑ ΤΑ ΔΕΔΟΜΕΝΑ ΤΟΥ TABLE ΑΠΟ ΤΟ STATE */}
-        {/* {this.state.customers.map(customer => 
+        {this.state.customers.map(customer => 
           <li key={customer.id}>{customer.firstName} {customer.lastName}</li>
-        )} */}
-        </ul>
+        )}
+        </ul> */}
+
+        <table>
+
+          <tbody>
+
+            {this.state.customers[0] && this.state.customers[0].map((customer, i)=> 
+
+              <tr key = {i}> 
+          
+                <td>{customer.prod_code}</td>
+                <td>{customer.category}</td>
+                <td>{customer.inventory}</td>
+                <td>{customer.rotation}</td>
+                <td>{customer.lifetime}</td>
+                <td>{customer.estimated_overstock}</td>
+                <td>{customer.value_at_risk}</td>
+                <td>{customer.current_price}</td>
+                <td>{customer.optimal_markdown}</td>
+                <td>{customer.attainable_increase_turnover}</td>
+                <td>{customer.attainable_increase_markdown}</td>
+        
+              </tr>
+             )}
+
+          </tbody>
+
+        </table>
+
       </div>
     );
   }
