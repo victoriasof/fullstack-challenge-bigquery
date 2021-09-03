cdd5c23cf3e2aec87890bafd88411f69e671c523
victoriasof <victoria.sofianidou@gmail.com>
2021-09-03 11:15:53 +0200

    having trouble with radio buttons

--- client/src/components/products.js
+++ client/src/components/products.js
@@ -6,6 +6,7 @@ class Products extends Component {
     super();
     this.state = {
       products: []
+   
     };
   }
 
@@ -18,6 +19,16 @@ class Products extends Component {
         this.setState({products}, () => console.log('products fetched...', products))
       });
   }
+
+  //examples for radio buttons: 
+  //https://www.codegrepper.com/code-examples/javascript/event+handler+in+react+radio+button
+  //https://www.codegrepper.com/code-examples/javascript/uncheck+to+find+value++radio+button+with+same+name+in+react
+
+  handleChange = e => {
+    const { name, value } = e.target;
+
+    this.setState({[name]: value});
+  };
 
   render() {
     //console.log(this.state.products);
@@ -67,15 +78,15 @@ class Products extends Component {
 
                 <td >
                   <div id="markdown">
-                    <input type="radio" value="current" /> Current
+                    <input type="radio" value="current" name="markdown" onChange={this.handleChange} /> Current
                   </div>
                   <div id="markdown">
                     {/* Warning: Failed prop type: You provided a checked prop to a form field without an onChange handler. This will render a read-only field. 
                     If the field should be mutable use defaultChecked. Otherwise, set either onChange or readOnly. */}
-                    <input type="radio" value="optimal" defaultChecked/> Optimal {product.optimal_markdown*100}%
+                    <input type="radio" value="optimal" name="markdown" defaultChecked onChange={this.handleChange} /> Optimal {product.optimal_markdown*100}%
                   </div>
                   <div id="markdown"> 
-                    <input type="radio" value="custom" /> Custom  
+                    <input type="radio" value="custom" name="markdown" onChange={this.handleChange} /> Custom  
                     <input type="text" id="markdown" name="percent" /> %
                     {/* [DOM] Found 12 elements with non-unique id #markdown.
                     I used the id for styling in products.css*/}
